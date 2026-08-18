41b4fd120d305de7b71dba72154b21094d26f750
m-farajian <m.farajian@cooltag.ir>
2026-08-18 17:37:33 +0330

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shamsi_date',
+    version='1.0.0',
+    description='Jalali (Shamsi) Date Converter for ERPNext',
+    author='Majid Farajian',
+    author_email='majidfa1999@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[]
+)
---
+++ shamsi_date/hooks.py
@@ -0,0 +1,8 @@
+app_name = "shamsi_date"
+app_include_js = "/assets/shamsi_date/js/shamsi_date.js"
+
+jinja = {
+    "methods": [
+        "shamsi_date.utils:get_shamsi_date",
+    ]
+}
