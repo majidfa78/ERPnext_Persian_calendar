8cc6186deb1c794baee0ae0d8a9cd93c99d2f5a5
m-farajian <m.farajian@cooltag.ir>
2026-08-18 18:30:00 +0330

    folders

--- ERPnext_Persian_calendar/hooks.py
+++
@@ -1,8 +0,0 @@
-app_name = "erpnext_persian_calendar"
-app_include_js = "/assets/erpnext_persian_calendar/js/persian_calendar.js"
-
-jinja = {
-    "methods": [
-        "erpnext_persian_calendar.utils:get_shamsi_date",
-    ]
-}
