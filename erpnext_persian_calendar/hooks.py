be5439acacad1250dbbdc80d43051a2a0dc29e9a
m-farajian <m.farajian@cooltag.ir>
2026-08-18 18:41:01 +0330

    test

--- erpnext_persian_calendar/hooks.py
+++ erpnext_persian_calendar/hooks.py
@@ -1,8 +1,12 @@
+# erpnext_persian_calendar/hooks.py
 app_name = "erpnext_persian_calendar"
-app_include_js = "/assets/erpnext_persian_calendar/js/persian_calendar.js"
+app_title = "Persian Calendar"
+app_publisher = "Majid Farajian"
+app_description = "Gregorian to Shamsi date converter"
+app_icon = "octicon octicon-calendar"
+app_color = "#3498db"
+app_email = "majidfa1999@gmail.com"
+app_license = "MIT"
 
-jinja = {
-    "methods": [
-        "erpnext_persian_calendar.utils:get_shamsi_date",
-    ]
-}
+# Include JS file
+app_include_js = "/assets/erpnext_persian_calendar/js/persian_calendar.js"
--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='erpnext_persian_calendar',
     version='1.0.0',
-    description='Jalali (Shamsi) Date Converter for ERPNext',
-    author='Majid Farajian',
-    author_email='majidfa1999@gmail.com',
     packages=find_packages(),
-    zip_safe=False,
     include_package_data=True,
-    install_requires=[]
+    zip_safe=False,
 )
